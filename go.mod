91ad57c9d8ff566c14dc6759a032c7609084c606
Scott Baker <smbaker@gmail.com>
2019-11-04 09:28:14 -0800

    VOL-2189 update voltha-simonu-adapter to use voltha-protos=v2.0.1 and voltha-lib-go=v2.2.9; Release 2.2.1
    Change-Id: I65b5736acfcb2bf47047105c5cd188a456dd5982

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.12
 
 require (
 	github.com/gogo/protobuf v1.3.0
-	github.com/opencord/voltha-lib-go/v2 v2.2.7
+	github.com/opencord/voltha-lib-go/v2 v2.2.9
 
-	github.com/opencord/voltha-protos v1.0.3
+	github.com/opencord/voltha-protos/v2 v2.0.1
 	github.com/stretchr/testify v1.4.0
 )
--- internal/pkg/adaptercore/simulated_onu.go
+++ internal/pkg/adaptercore/simulated_onu.go
@@ -22,9 +22,9 @@ import (
 	com "github.com/opencord/voltha-lib-go/v2/pkg/adapters/common"
 	"github.com/opencord/voltha-lib-go/v2/pkg/kafka"
 	"github.com/opencord/voltha-lib-go/v2/pkg/log"
-	ic "github.com/opencord/voltha-protos/go/inter_container"
-	"github.com/opencord/voltha-protos/go/openflow_13"
-	"github.com/opencord/voltha-protos/go/voltha"
+	ic "github.com/opencord/voltha-protos/v2/go/inter_container"
+	"github.com/opencord/voltha-protos/v2/go/openflow_13"
+	"github.com/opencord/voltha-protos/v2/go/voltha"
 	"sync"
 )
 
